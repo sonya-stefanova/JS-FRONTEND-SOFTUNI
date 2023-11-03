35da4c2254605f9cec94f2b9c61b318b85813381
Sonya <sonya_deanova@yahoo.com>
2023-11-03 16:38:48 +0200

    folder initialised

---
+++ 01_JS_SYNTAX_LAB/2_student_info.js
@@ -0,0 +1,7 @@
+function student_info(name, age,grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+student_info('Ivanka', 18, 6.00)
+student_info('Milena', 19, 5.00)
+student_info('Pesho', 17, 5.050)
