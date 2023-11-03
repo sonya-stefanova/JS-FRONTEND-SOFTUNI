2a330af7f5a614867fce0f871e4e6a8eeb2208a2
Sonya <sonya_deanova@yahoo.com>
2023-11-03 16:28:47 +0200

    folder initialised

---
+++ 01_JS_SYNTAX_LAB/1_Multiply the Number by 2.js
@@ -0,0 +1,8 @@
+function multiplyBy2(num) {
+    console.log(num*2);
+
+}
+
+multiplyBy2(2);
+multiplyBy2(24);
+multiplyBy2(3);
---
+++ tst.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 6;
+// if (a > b) {
+//     console.log("a>b of course")
+// } else {
+//     console.log("b is bigger")
+// }
+
+function test(startMessage) {
+    return startMessage + ' hello again'
+}
+
+console.log(test('123'))
