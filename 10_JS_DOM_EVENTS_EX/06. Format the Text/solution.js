67533c77d9df741ee9cd2209eeccee1cff82588a
Sonya <sonya_deanova@yahoo.com>
2023-11-20 01:26:55 +0200

    solutions added

--- 10_JS_DOM_EVENTS_EX/06. Format the Text/solution.js
+++ 10_JS_DOM_EVENTS_EX/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+  const input = document.querySelector("#input").value.split(".");
+  // the last element after the fullstop is cleared;
+  input.pop();
+  const container = document.querySelector("#output");
+
+  while (input.length > 0) {
+    const p = document.createElement("p");
+    p.textContent = input
+      .splice(0, 3)
+      .map((text) => (text.trim()) + '.')
+      .join(".");
+    container.appendChild(p);
+  };
+
 }
