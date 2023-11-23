31e06138fe8d40e7ab30b475e364b6874f92752e
Sonya <sonya_deanova@yahoo.com>
2023-11-23 16:55:35 +0200

    adding DOM ex solutions

--- 10_JS_DOM_EVENTS_EX/09. Number Convertor/solution.js
+++ 10_JS_DOM_EVENTS_EX/09. Number Convertor/solution.js
@@ -1,4 +1,22 @@
 function solve() {
+    let resultElement = document.querySelector("#result");
+    let selectMenuElement = document.querySelector("#selectMenuTo");
+    let buttonElement = document.querySelector("button");
 
-    //TODO...
-}
+    const optionCreator = (content, value) => {
+        let option = document.createElement("option");
+        option.innerHTML = content;
+        option.value = value;
+        return option;
+    }    
+
+    selectMenuElement.appendChild(optionCreator("Binary", "binary"));
+    selectMenuElement.appendChild(optionCreator("Hexadecimal", "hexadecimal"));
+
+    buttonElement.addEventListener("click", () => {
+        let numberToConvert = Number(document.querySelector("#input").value);
+
+        let selectedConverter = selectMenuElement.value === "binary" ? 2 : 16;
+        resultElement.value = numberToConvert.toString(selectedConverter).toUpperCase();
+    });
+};
