a325c9027cd817d644973fb1849161edd4f63628
Sonya <sonya_deanova@yahoo.com>
2023-11-28 13:05:17 +0200

    REST ex added

--- 12_JS_HTTP_REST_EX/01.Bus-Stop/app.js
+++ 12_JS_HTTP_REST_EX/01.Bus-Stop/app.js
@@ -1,3 +1,73 @@
 function getInfo() {
-    // TODO:
-}
+    const busStopID = document.querySelector('#stopId').value;
+    const ulElement = document.querySelector('ul');
+    const stopName = document.querySelector('#stopName');
+
+    ulElement.innerHTML = "";
+
+    const apiManager = {
+        busStopURL: (ID) => `http://localhost:3030/jsonstore/bus/businfo/${ID}`
+    }
+
+    fetch(apiManager.busStopURL(busStopID))
+        .then(res => res.json())
+        // .then(console.log);
+        .then((busStop) =>{
+            stopName.textContent = busStop.name;
+
+
+            Object.entries(busStop.buses).map(([busNumber, timeInMins])=>{
+                const listItem = document.createElement('li');
+                listItem.textContent = `Bus ${busNumber} arrives in ${timeInMins} minutes`
+
+                // document.querySelector('ul').appendChild(listItem); 
+
+                ulElement.appendChild(listItem);
+            })
+
+        })
+
+        .catch(() => {
+            stopName.textContent = "Error";
+        });
+
+    };
+// async function getInfo() {
+//     const busStopID = document.querySelector('#stopId').value;
+//     const ulElement = document.querySelector('ul');
+//     const stopName = document.querySelector('#stopName');
+
+//     ulElement.innerHTML = "";
+
+//     const apiManager = {
+//         busStopURL: (ID) => `http://localhost:3030/jsonstore/bus/businfo/${ID}`
+//     }
+
+//     try{
+//         const response = await fetch(apiManager.busStopURL(busStopID));
+//         const busStopInfo = await response.json();            
+
+//     } catch(_) {
+//         stopName.textContent = "Error";
+//         return;
+//     };
+
+//     stopName.textContent = busStopInfo.name;
+
+//     Object.entries(busStopInfo.buses).map(([busNumber, timeInMins])=>{
+//         const listItem = document.createElement('li');
+//         listItem.textContent = `Bus ${busNumber} arrives in ${timeInMins} minutes`
+
+//         // document.querySelector('ul').appendChild(listItem); 
+
+//         ulElement.appendChild(listItem);
+//     });
+
+    
+// };
+
+
+
+
+
+
--- 12_JS_HTTP_REST_EX/02.Bus-Schedule/app.js
+++ 12_JS_HTTP_REST_EX/02.Bus-Schedule/app.js
@@ -1,10 +1,35 @@
 function solve() {
+
+    const departButton = document.querySelector('#depart');
+    const arriveButton = document.querySelector('#arrive');
+    const busInfoBox = document.querySelector('#info .info');
+
+    let busStopInfo = {
+        name: "",
+        next: "depot",
+    };
+
     function depart() {
-        // TODO: 
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${busStopInfo.next}`)
+        .then(result => result.json())
+        .then((busStop) =>{
+            busStopInfo  = busStop;
+            departButton.disabled = true;
+            arriveButton.disabled = false;
+            busInfoBox.textContent = `Next stop ${busStopInfo.name}`;
+        })
+        .catch(() => {
+            departButton.disabled = true;
+            arriveButton.disabled = true;
+            busInfoBox.textContent = `Error`;
+        });
     }
 
     async function arrive() {
-        // TODO:
+        departButton.disabled = false;
+        arriveButton.disabled = true;
+        busInfoBox.textContent = `Arriving at ${busStopInfo.name}`;
+
     }
 
     return {
